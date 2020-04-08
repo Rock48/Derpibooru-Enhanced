ca60ca9f996d0ec4c3c4f0f58d6e9b450f164242
Charles Quigley <cquiglz@gmail.com>
2020-04-08 08:45:11 -0400

    version bump + readme update

--- derpibooru.enhanced.user.js
+++ derpibooru.enhanced.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Derpibooru Enhanced
 // @namespace    http://moonlightsoftware.net/
-// @version      0.2.9
+// @version      0.3.0
 // @description  Adds some new features to derpibooru!
 // @author       Charles "Rock48" Quigley
 // @match        https://derpibooru.org/*
